007cce5103fb99d8c884313b6a54bb1eb35f1642
Kate Eri <katherinmail@gmail.com>
2017-03-02 17:32:31 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -x
+set -o nounset \
+    -o errexit \
+    -o verbose \
+    -o xtrace
 
 # If a ZooKeeper container is linked with the alias `zookeeper`, use it.
 # You MUST set ZOOKEEPER_IP in env otherwise.
-[ -n "$ZOOKEEPER_PORT_2181_TCP_ADDR" ] && ZOOKEEPER_IP=$ZOOKEEPER_PORT_2181_TCP_ADDR
-[ -n "$ZOOKEEPER_PORT_2181_TCP_PORT" ] && ZOOKEEPER_PORT=$ZOOKEEPER_PORT_2181_TCP_PORT
 
 IP=$(grep "\s${HOSTNAME}$" /etc/hosts | head -n 1 | awk '{print $1}')
 
 # Concatenate the IP:PORT for ZooKeeper to allow setting a full connection
 # string with multiple ZooKeeper hosts
-[ -z "$ZOOKEEPER_CONNECTION_STRING" ] && ZOOKEEPER_CONNECTION_STRING="${ZOOKEEPER_IP}:${ZOOKEEPER_PORT:-2181}"
+[[ -n ${ZOOKEEPER_PORT_2181_TCP_ADDR:-} ]] && ZOOKEEPER_IP=$ZOOKEEPER_PORT_2181_TCP_ADDR	
+[[ -n ${ZOOKEEPER_PORT_2181_TCP_PORT:-} ]] && ZOOKEEPER_PORT=$ZOOKEEPER_PORT_2181_TCP_PORT
+[[ -z ${ZOOKEEPER_CONNECTION_STRING:-} ]] && ZOOKEEPER_CONNECTION_STRING="${ZOOKEEPER_IP}:${ZOOKEEPER_PORT:-2181}"
 
 cat /kafka/config/server.properties.template | sed \
   -e "s|{{ZOOKEEPER_CONNECTION_STRING}}|${ZOOKEEPER_CONNECTION_STRING}|g" \
@@ -37,7 +41,7 @@ cat /kafka/config/server.properties.template | sed \
 # hosts running in a VM with Docker Machine, etc. See:
 #
 # https://issues.apache.org/jira/browse/CASSANDRA-7087
-if [ -z $KAFKA_JMX_OPTS ]; then
+if [[ -z ${KAFKA_JMX_OPTS:-} ]]; then
     KAFKA_JMX_OPTS="-Dcom.sun.management.jmxremote=true"
     KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.authenticate=false"
     KAFKA_JMX_OPTS="$KAFKA_JMX_OPTS -Dcom.sun.management.jmxremote.ssl=false"
